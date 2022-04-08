b745d906245a621e2ae5e9a442a165039a275f29
zaksk <zakariamar.99@gmail.com>
2022-04-08 12:12:37 +0000

    test.sh

--- test.sh
+++ test.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 declare -a directories=("service-1" "service-2" "service-3" "service-4")
+sudo apt update
+sudo apt install python3 python3-pip python3.8-venv
+python3 -m venv venv
+source venv/bin/activate
+pip3 install -r test_requirements.txt
 for dir in "${directories[@]}"
 do
   cd ${dir}
-  sudo apt-get update
-  sudo apt-get install python3 python3-pip python3.8-venv
-  python3 -m venv venv
-  source venv/bin/activate
-  pip3 install -r test_requirements.txt
   python3 -m pytest --cov=application --cov-report term-missing --cov-report=html -p no:warnings
-  deactivate
   cd ..
 done
+deactivate
